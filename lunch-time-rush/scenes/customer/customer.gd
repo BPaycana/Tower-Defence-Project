da47bf44fcd0126bb05674602fc158867465409c
Brent Paycana <brent.paycana@gmail.com>
2025-01-19 16:01:36 +1100

    V0.18
    + Reorganized the project
    
    (PLEASE DONT END UP BEING BROKEN)

---
+++ lunch-time-rush/scenes/customer/customer.gd
@@ -0,0 +1,187 @@
+extends PathFollow2D
+
+@onready var sprites: AnimatedSprite2D = $Sprites
+@onready var emotes: AnimatedSprite2D = $Emotes
+
+@export var speed = 50
+@export var hp: int = 3
+
+@export var food_amount: RichTextLabel
+@export var container: VBoxContainer
+
+@onready var sfx_eating: AudioStreamPlayer = $SFXEating
+@onready var sfx_full: AudioStreamPlayer = $SFXFull
+
+
+const BURGER = preload("res://scenes/ui/food_bubble/burger_gui.tscn")
+const FRIES = preload("res://scenes/ui/food_bubble/fries_gui.tscn")
+const DRINK = preload("res://scenes/ui/food_bubble/drink_gui.tscn")
+
+var text_effect: String = "[color=black][wave amp=10.0 freq=10.0 connected=1] "
+
+var skin: int
+
+var direction
+var rounded_direction
+var anim_finished = true
+
+var food_type = FoodType
+var has_drink = false
+
+var is_satisfied = false
+
+var level
+
+func _ready() -> void:
+	direction = position
+	level = get_parent().get_parent()
+	add_food_to_container()
+	food_amount.text = text_effect + str(hp)
+	skin = int(randf_range(1, 3))
+	
+func _physics_process(delta: float) -> void:
+	move(delta)
+	if progress_ratio >= 1:
+		delete()
+
+func move(delta):
+	set_progress(get_progress() + speed * delta)
+	
+	var old_pos = position
+	await(get_tree().create_timer(0.1).timeout)
+	direction = position - old_pos
+	rounded_direction = round_direction(direction)
+	print_debug(rounded_direction)
+	
+	
+	
+	match rounded_direction:
+		Vector2.LEFT:
+			if anim_finished:
+				sprites.play("walk_left" + str(skin))
+		Vector2.RIGHT:
+			if anim_finished:
+				sprites.play("walk_right" + str(skin))
+		Vector2.DOWN:
+			if anim_finished:
+				sprites.play("walk_down" + str(skin))
+		Vector2.UP:
+			if anim_finished:
+				sprites.play("walk_up" + str(skin))
+		Vector2.DOWN + Vector2.LEFT:
+			if anim_finished:
+				sprites.play("walk_down" + str(skin))
+		Vector2.DOWN + Vector2.RIGHT:
+			if anim_finished:
+				sprites.play("walk_down" + str(skin))
+		Vector2.UP + Vector2.LEFT:
+			if anim_finished:
+				sprites.play("walk_up" + str(skin))
+		Vector2.UP + Vector2.RIGHT:
+			if anim_finished:
+				sprites.play("walk_up" + str(skin))
+
+func round_direction(direction: Vector2) -> Vector2:
+	var x = direction.x
+	var y = direction.y
+
+	return Vector2(1 if x > 0 else -1 if x < 0 else 0, 1 if y > 0 else -1 if y < 0 else 0)
+
+func on_hit(_food_type, _damage):
+	if !is_satisfied:
+		if _food_type == food_type:
+			hp -= _damage
+			food_amount.text = text_effect + str(hp)
+			hurt_anim()
+		
+		if _food_type.name == "Drink":
+			if has_drink:
+				hp -= _damage
+				food_amount.text = text_effect + str(hp)
+				hurt_anim()
+		
+		if hp > 0:
+			sfx_eating.play()
+
+		if hp <= 0:
+			satisfied()
+
+func satisfied():
+	is_satisfied = true
+	
+	#Change visibility of bubbles
+	container.get_parent().set_visible(false)
+	emotes.set_visible(true)
+	emotes.play("satisfied")
+	sfx_full.play()
+
+	speed = speed * 5
+
+func delete():
+	level.customers_in_wave -= 1
+	
+	if level.customers_in_wave <= 0:
+		level.button.set_visible(true)
+	if !is_satisfied:
+		level.customer_unsatisfied()
+	queue_free()
+
+func hurt_anim():
+
+	anim_finished = false
+
+	match rounded_direction.normalized():
+		Vector2.LEFT:
+			sprites.play("hurt_left" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.RIGHT:
+			sprites.play("hurt_right" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.DOWN:
+			sprites.play("hurt_down" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.UP:
+			sprites.play("hurt_up" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.DOWN + Vector2.LEFT:
+			sprites.play("hurt_down" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.DOWN + Vector2.RIGHT:
+			sprites.play("hurt_down" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.UP + Vector2.LEFT:
+			sprites.play("hurt_up" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+		Vector2.UP + Vector2.RIGHT:
+			sprites.play("hurt_up" + str(skin))
+			await sprites.animation_finished
+			anim_finished = true
+
+func add_food_to_container():
+	if food_type == null:
+		return
+
+	match food_type.name:
+		"Burger": 
+			var burger = BURGER.instantiate()
+			container.add_child(burger)
+		
+		"Fries":
+			var fries = FRIES.instantiate()
+			container.add_child(fries)
+
+	if has_drink:
+		var drink = DRINK.instantiate()
+		container.add_child(drink)
+
+func set_customer(_food_type: FoodType, _food_amount: int, _has_drink: bool):
+	food_type = _food_type
+	hp = _food_amount
+	has_drink = _has_drink
---
+++ lunch-time-rush/scenes/levels/level.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+@export var path1: Path2D
+@export var path2: Path2D
+
+@export var fries: FoodType
+@export var burger: FoodType
+
+@export var star_container: HBoxContainer
+
+@export var level_data: LevelData
+
+@onready var button: Button = $CanvasLayer/Button
+
+var wave_data = null
+var wave_number: int = 0
+var customers_in_wave = 0
+
+var max_stars: int = 5
+var current_stars: int
+
+func _ready() -> void:
+	current_stars = max_stars
+	star_container.set_max_stars(max_stars)
+	star_container.update_stars(current_stars)
+	
+
+func start_wave():
+	
+	if level_data == null:
+		print("No wave data assigned!")
+		return
+		
+	wave_data = retrieve_wave_data()
+	
+	for customer_data in wave_data.customers:
+		spawn_customer(customer_data)
+		await get_tree().create_timer(wave_data.spawn_delay).timeout
+
+func retrieve_wave_data():
+	var new_wave_data = level_data.wave[wave_number]
+	wave_number += 1
+	customers_in_wave = new_wave_data.customers.size()
+	return new_wave_data
+
+func decide_path() -> Path2D:
+	if randi() % 2 == 0:
+		return path1
+	else:
+		return path2
+
+func spawn_customer(customer_data):
+
+	var new_customer = load("res://scenes/customer/customer.tscn").instantiate()
+	new_customer.set_customer(customer_data.food_type, customer_data.hp, customer_data.has_drink)
+	decide_path().add_child(new_customer)
+
+func customer_unsatisfied():
+	current_stars -= 1
+	star_container.update_stars(current_stars)
+	if current_stars <= 0:
+		print_debug("You Lose!")
+
+func _on_button_button_down() -> void:
+	if wave_number < level_data.wave.size():
+		button.set_visible(false)
+		start_wave()
